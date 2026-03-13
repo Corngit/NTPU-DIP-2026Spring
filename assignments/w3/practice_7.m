27bf9d7e74402e347bfd12dc9dfd6ed7e8ef75dc
Corngit <erichym0407@gmail.com>
2026-03-13 09:58:03 +0800

    w3 Commit

--- assignments/w3/practice_7.m
+++ assignments/w3/practice_7.m
@@ -1,5 +1,7 @@
 c = imread('../../data/Original_Images_1/caribou.jpg');
 cd = double(c);
+cd2 = double(cd/255);
 
+figure, imshow(cd2), impixelinfo
 figure, imshow(c), impixelinfo
 figure, imshow(cd), impixelinfo
